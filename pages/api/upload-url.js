9a90bf30e822cc6550f28398b055fcb2e7dcafac
Lee Robinson <lrobinson2011@gmail.com>
2020-11-01 20:30:59 -0600

    1mb limit.
    https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-HTTPPOSTConstructPolicy.html

--- pages/api/upload-url.js
+++ pages/api/upload-url.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     },
     Expires: 60, // seconds
     Conditions: [
-      ['content-length-range', 0, 8000000], // up to 1 MB
+      ['content-length-range', 0, 1048576], // up to 1 MB
     ],
   });
 
